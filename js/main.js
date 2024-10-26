917600dcb74b6b5b0dd3804940f5405a0c2b27db
GeisilaCosta <geisilascosta@gmail.com>
2024-10-26 19:16:38 -0300

    iniciando o projeto

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function callEmergency() {
+    const emergencyNumber = "193"; // Número dos Bombeiros no Brasil
+    window.location.href = `tel:${emergencyNumber}`;
+    alert("Chamando os Bombeiros...");
+  }
+  
